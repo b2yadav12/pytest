694b91e7915b54e7101d74541396fdb9484c436a
Bittu Yadav <bittu@aeologic.com>
2019-08-12 12:53:10 +0530

    add crud

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,8 +5,12 @@ from resp import views as resp_views
 
 
 urlpatterns = [
-    path('html/', resp_views.html),
+    path('', resp_views.html),
     path('json/', resp_views.json),
+    path('save-persion', resp_views.createPerson),
+    path('persion-list', resp_views.personList),
+    path('search-persion/<first_name>', resp_views.filterPersion),
+    path('get-persion/<id>', resp_views.getPersion),
     path('resp/', include('resp.urls')),
     path('admin/', admin.site.urls),
 ]
--- resp/views.py
+++ resp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from resp.models import Person
 
 def html(request):
     response = HttpResponse()
@@ -9,3 +10,47 @@ def html(request):
 
 def json(request):
     return JsonResponse({'foo': 'bar'})
+
+def createPerson(request):
+    person=Person(first_name='Amit', last_name='Singh')
+    person.save()
+
+    response = HttpResponse()
+    response.write("<h2><center>New Person Saved Succeessfully</center></h2>")
+    return response
+
+def personList(request):
+    persons=Person.objects.all()
+    print("Person List Start")
+
+    for row in persons:
+        print(str(row.id)  + " : " + row.first_name + " " + row.last_name)
+
+    print("Person List End")
+    response = HttpResponse()
+    response.write("<h2><center>Querysets are printed in terminal</center></h2>")
+    return response
+
+def filterPersion(request, first_name):
+    print("param : " + first_name)
+    persons=Person.objects.filter(first_name=first_name).all()
+    print("Person List Start")
+
+    for row in persons:
+        print(str(row.id)  + " : " + row.first_name + " " + row.last_name)
+
+    print("Person List End")
+    response = HttpResponse()
+    response.write("<h2><center>Querysets are printed in terminal</center></h2>")
+    return response
+
+def getPersion(request, id):
+    person=Person.objects.get(id=id)
+    print("Person row start")
+
+    print(str(person.id)  + " : " + person.first_name + " " + person.last_name)
+
+    print("Person row End")
+    response = HttpResponse()
+    response.write("<h2><center>Person details are printed in terminal</center></h2>")
+    return response
